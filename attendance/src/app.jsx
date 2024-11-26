6a59c2b54651790575858168ad5e337a559048ab
Admin A. <adadmin@sscet.edu.local>
2024-11-26 13:54:02 +0530

    init committed

---
+++ attendance/src/app.jsx
@@ -0,0 +1,9 @@
+import { Day1 } from "./components/Day1"
+
+function app() {
+    return (<>
+        <Day1/>
+    </>
+    )
+}
+export default app
---
+++ attendance/src/components/Day1.jsx
@@ -0,0 +1,12 @@
+
+export const Day1 = () => {
+  return (
+    <div>
+        <h1 className="Header">Day 1 tasks</h1>
+        <p>What is React ?</p>
+        <p>JSX ?</p>
+        <p>create-react-app</p>
+        <p>components ?</p>
+    </div>
+  )
+}
